41da3101d8a5d6efb140291f156b5c591322840c
User Name <jessnsq.codes@gmail.com>
2023-08-21 14:46:53 -0400

    functionality updates

--- game.js
+++ game.js
@@ -465,6 +465,7 @@ let howToPLay = "Try to collect as many coins for maximum points while trying to
 // username()
 
 
+
 let box1= document.querySelector('div.box')
 let boxDimensions = box1.getBoundingClientRect()
 console.log(boxDimensions)
@@ -535,38 +536,47 @@ let timer = setInterval(function(){
 
 //___________________________________________________________________________________________________________________________________________________________________________
 
-//BUILDINGS
-let build1 = document.querySelector(".buid1")
-let build2 = document.querySelector(".buid2")
-let build3 = document.querySelector(".buid3")
-let build4 = document.querySelector(".buid4")
-let build5 = document.querySelector(".buid5")
+//BUILDING
 let build6 = document.querySelector('.buid6')
-let build7 = document.querySelector(".buid7")
-let build8 = document.querySelector(".buid8")
-let build9 = document.querySelector(".buid9")
-let build10 = document.querySelector(".buid10")
 
 
 //________________________________________________________________________________________________________________________________________________________________________________
 //VARIABLES & ARRAYS SET FOR EASY ACCESS 
-
-//IMAGES
+// //IMAGES
 
 let buildingImgs = [   //create an array of images 
-"https://static.wixstatic.com/media/4c3267_63836149e75e47dc8e1bd942c41211ad~mv2.png", //build1  
-"https://static.wixstatic.com/media/4c3267_aed2ca0a426f4a4d87dee0f80a582a3d~mv2.png", //build2
-"https://static.wixstatic.com/media/4c3267_63836149e75e47dc8e1bd942c41211ad~mv2.png", //build3
-"https://static.wixstatic.com/media/4c3267_7f9457c5386d4c3fa8246edc08502153~mv2.png", //statue1 
-"https://static.wixstatic.com/media/4c3267_f570fc50014c4a70a4209da47341eaa9~mv2.png",  
-"https://static.wixstatic.com/media/4c3267_e4c7f702bc5e46739f255a455bea693c~mv2.png", // bridge
-"https://static.wixstatic.com/media/4c3267_0d9d7d152ebf4a429dbab8b2cff18a8a~mv2.png",
-"https://static.wixstatic.com/media/4c3267_d4dcd19afc3a44c291a20b4fd5c5c988~mv2.png",
-"https://static.wixstatic.com/media/4c3267_52058fbd688e44578d5fe79aceca4c09~mv2.png",
-"https://static.wixstatic.com/media/4c3267_f2a00f62460742bebbda5a47d313402b~mv2.png",
-//**ADD/CHANGE IMAGES ONCE CODE WORKS
+"https://static.wixstatic.com/media/4c3267_3b8f93f1b7874eecacdc4db943ea4b72~mv2.jpg", //build1  
+"https://static.wixstatic.com/media/4c3267_78ed8701f9e14b02ac70a562167db996~mv2.png", //build2
+"https://static.wixstatic.com/media/4c3267_1c7c77d72d174d5fa3697ac68ae876d1~mv2.png", //build3
+'https://static.wixstatic.com/media/4c3267_7de0dfdb01ed4969af7d501196785181~mv2.png',
+
 ]
 
+//CHANGE IMAGES INTERCHANGEBLY -- Created two setIntervals to make the game difficult 
+let imgs = 0;  //Assign variable starting at 0
+let imgtimer = setInterval(function(){
+    console.log("Skyscrapper2 approaching")
+    imgs++;             
+
+    if(imgs === 6) {                                     
+        document.querySelector(".buid6").src = buildingImgs[0];  
+        console.log("image2 changed succesfully")                   
+    }
+    if(imgs === 10) {
+        document.querySelector(".buid6").src = buildingImgs[1];  
+        console.log("image2 changed succesfully")  
+     }
+
+     if(imgs === 12) {                                   
+        document.querySelector(".buid6").src = buildingImgs[2];  
+        console.log("image2 changed succesfully") 
+     }      
+
+     if(imgs === 18) {                                   
+        document.querySelector(".buid6").src = buildingImgs[3];  
+        console.log("image2 changed succesfully") 
+     }
+}, 3000);
 //______________________________________________________________________________________________________________________
 
 
@@ -594,153 +604,6 @@ const fly = () => {
          
   }
  })
-
-
-// //HERO
-
-// let heroDim = hero.getBoundingClientRect()
-// let heroHeight = heroDim.height
-// console.log(heroHeight)
-// let heroWidth = heroDim.width
-// console.log(heroWidth)
-
-
-
-// // BUILDING 6 DIMENSIONS
-// //Get Dimensions: Height & Width 
-// let buildDim6 = build6.getBoundingClientRect()
-// let build6Height = buildDim6.height
-// let build6Width = buildDim6.width
-// console.log(buildDim6)
-
-
-
-// //Function below checks if both objects (hero & building) have collided 
-// let death = setInterval(function(){
-
-//     const heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue('top'))
-//     console.log(heroTop)
-
-//     const build6lftie= parseInt(window.getComputedStyle(build6).getPropertyValue('left'))
-//     console.log(build6lftie)
-
-    
-//         console.log(heroWidth)
-//         console.log(build6Width)
-//         console.log(heroHeight)
-//         console.log(build6Height)
-
-//     if (heroTop >= 545 && heroWidth <= build6Width && heroHeight<=build6Height && build6lftie < heroWidth){   //I'm now using different dimensions to check if they've collided. These are more accurate 
-//          build6.style.animation = "none";
-//           console.log("Dead6")
-//           clearInterval(death)//stops animation
-//           clearInterval(timer) //stops timer 
-//           clearInterval(imgtimer)   
-//           clearInterval(imgtimer2)                  
-//           clearInterval(coinPoints)//stops coins 
-//           alert("Superman has died!!")
-//         } 
-//     }, 2000);
-
-
-//CHANGE IMAGES -- Using images from BuildingImgs array and changing them automatically 
-
-// let imgs = 0;  //Assign variable starting at 0
-// let imgtimer = setInterval(function(){
-//     console.log("Skyscrapper approaching")
-//     imgs++;             
-
-//     if(imgs === 4) {                                     
-//         document.querySelector(".buid6").src = buildingImgs[1];  
-//         console.log("image changed succesfully")                   
-//     }
-//     if(imgs === 6) {
-//         document.querySelector(".buid6").src = buildingImgs[2];  
-//         console.log("image changed succesfully")  
-//      }
-
-//      if(imgs === 9) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[3];  
-//         console.log("image changed succesfully") 
-//      }      
-
-//      if(imgs === 10) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[4];  
-//         console.log("image changed succesfully") 
-//      }
-//      if(imgs === 15) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[1];  
-//         console.log("image changed succesfully") 
-//      }
-//      if(imgs === 25) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[6];  
-//         console.log("image changed succesfully") 
-//      }
-//      if(imgs === 30) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[7];  
-//         console.log("image changed succesfully") 
-//      }
-//      if(imgs === 32) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[8];  
-//         console.log("image changed succesfully") 
-//      }
-//     if(imgs === 35) {                                   
-//             document.querySelector(".buid6").src = buildingImgs[9];  
-//             console.log("image changed succesfully") 
-//         }
-// }, 1500);
-
-
-
-// //CHANGE IMAGES INTERCHANGEBLY -- Created two setIntervals 
-// let imgs2 = 0;  //Assign variable starting at 0
-// let imgtimer2 = setInterval(function(){
-//     console.log("Skyscrapper2 approaching")
-//     imgs++;             
-
-//     if(imgs2 === 8) {                                     
-//         document.querySelector(".buid6").src = buildingImgs[9];  
-//         console.log("image2 changed succesfully")                   
-//     }
-//     if(imgs2 === 12) {
-//         document.querySelector(".buid6").src = buildingImgs[10];  
-//         console.log("image2 changed succesfully")  
-//      }
-
-//      if(imgs2 === 16) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[6];  
-//         console.log("image2 changed succesfully") 
-//      }      
-
-//      if(imgs2 === 19) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[1];  
-//         console.log("image2 changed succesfully") 
-//      }
-
-// //CHANGE IMAGES FASTER, MAKING IT DIFFICULT TO WIN 
-
-//      if(imgs2 === 24) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[8];  
-//         console.log("image2 changed succesfully") 
-//      }
-//      if(imgs2 === 27) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[3];  
-//         console.log("image2 changed succesfully") 
-//      }
-//      if(imgs2 === 28) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[5];  
-//         console.log("image2 changed succesfully") 
-//      }
-//      if(imgs2 === 31) {                                   
-//         document.querySelector(".buid6").src = buildingImgs[2];  
-//         console.log("image2 changed succesfully") 
-//      }
-//     if(imgs2 === 33) {                                   
-//             document.querySelector(".buid6").src = buildingImgs[4];  
-//             console.log("image2 changed succesfully") 
-//         }
-// }, 3000);
-
 
 //___________________________________________________________________________________________________________________________________
 
@@ -786,8 +649,7 @@ let death = setInterval(function(){
           console.log("Dead6")
           clearInterval(death)//stops animation
           clearInterval(timer) //stops timer 
-          clearInterval(imgtimer)   
-          clearInterval(imgtimer2)                  
+          clearInterval(imgtimer)                 
           clearInterval(coinPoints)//stops coins 
           alert("Superman has died!!")
         } 
@@ -829,15 +691,17 @@ let coinPoints = setInterval(function(){
     const coinlftie= parseInt(window.getComputedStyle(coin).getPropertyValue('left'))
     console.log(coinlftie)
 
- if (heroTop >= 545 && heroWidth >= coinWidth && heroHeight >= coinHeight && coinlftie < -820  && coinlftie < -800 ){
+    //  && coinlftie > -1900 && heroWidth <= coinWidth && heroHeight <= coinHeight
+
+ if (heroTop >= 790 && coinlftie < -1690){
     coinStart+= 1
     document.querySelector('#Coins').innerHTML =  "Coins: " + coinStart 
     }
     
 
-     if(coinStart === 10) {
+     if(coinStart == 10) {
         changeBackg(boxBackg)
-        alert("You're a winner!")
+        alert("You Win!")
         clearInterval(coinPoints)
         coin.style.animation = "none";
        clearInterval(timer)
